b36ce69cbd06a71a1bab1d1509adb0c8db2d3126
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2023-11-23 19:24:03 -0500

    2. Agregando librerias, struct y funciones

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <sstream>
+#include <functional>
 
 using namespace std;
+
+struct Carretera {
+    
+};
+
+struct CarreteraHash {
+    
+};
+
+struct CarreteraEqual {
+    
+};
+
+string reconstruye() {
+    return NULL;
+}
 
 int main()
 {
