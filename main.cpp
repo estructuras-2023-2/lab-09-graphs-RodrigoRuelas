47505d744c4bbcf3889cf4eaea8e641b8ae13935
RodrigoRuelas <103969147+RodrigoRuelas@users.noreply.github.com>
2023-11-23 19:42:05 -0500

    3. Implementando definicion de las struct's

--- main.cpp
+++ main.cpp
@@ -10,15 +10,23 @@
 using namespace std;
 
 struct Carretera {
-    
+    string id;
+    string ciudad1;
+    string ciudad2;
+    int costo;
+    Carretera(string id, string ciudad1, string ciudad2, int costo) : id(id), ciudad1(ciudad1), ciudad2(ciudad2),
 };
 
 struct CarreteraHash {
-    
+    size_t operator()(const Carretera& c) const {
+        return hash<string>()(c.id);
+    }    
 };
 
 struct CarreteraEqual {
-    
+    bool operator()(const Carretera& a, const Carretera& b) const {
+        return a.id == b.id;
+    }
 };
 
 string reconstruye() {
